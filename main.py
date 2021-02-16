9bb23f26c87d8848773fa569748ee7fdeb9b80e9
TensorVince <77510703+TensorVince@users.noreply.github.com>
2021-02-16 14:28:37 +0100

    Updated running sample in main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-# ********************************************************************************************************************************
+# --------------------------------------------------------------------------------------------------------------------------------
 # Author: TensorVince
-# License: Peaceful Open Source License (https://raw.githubusercontent.com/TensorVince/PeacefulOpenSourceLicense/main/LICENSE)
+# License: Peaceful Open Source Proxy License (POSPL)
+# License Url: https://raw.githubusercontent.com/TensorVince/PeacefulOpenSourceLicense/master/POSPL.txt
 # --------------------------------------------------------------------------------------------------------------------------------
 # Description: class to calculate the N next neighbours of a point
-# ********************************************************************************************************************************
+# --------------------------------------------------------------------------------------------------------------------------------
 from point import *
 from dataset import *
 from knn import *
@@ -13,7 +14,7 @@ from knn import *
 a = Point(2,4)
 b = Point(3,9)
 result = b - a
-print(result)
+print(f"a - b = {result}")
 
 # Test: Dataset
 classes = ["dogs", "cats"]
@@ -21,18 +22,20 @@ dsTest = DataSet(0, "red")
 dsTest.AddPoint(Point(2, 4))
 dsTest.AddPoint(Point(3, 8))
 
-for point in dsTest:
-    print(point)
-
-# Test: Dataset Container
 dsTest2 = DataSet(1, "blue")
 dsTest2.AddPoint(Point(2, 8))
 dsTest2.AddPoint(Point(3, 27))
 
+for dsClass, point in dsTest:
+    print(f"Class: {dsClass}, Point: {point}")
+
+for dsClass, point in dsTest2:
+    print(f"Class: {dsClass}, Point: {point}")
+
+# Test: Dataset Container
 dsc = DataSetContainer()
 dsc.AddDataSet(dsTest)
 dsc.AddDataSet(dsTest2)
-
 
 # Test: KNN
 print("TEST:KNN")
